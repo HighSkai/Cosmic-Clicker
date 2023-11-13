c86b0ebee4ae66952737f3dcd7983bad0d3b4a3d
Skai <skai.ronis1@gmail.com>
2023-11-12 21:52:21 -0500

    Added Rebirth Menu

--- Game/script.js
+++ Game/script.js
@@ -6,33 +6,47 @@ document.addEventListener('DOMContentLoaded', function () {
     const settingsContainer = document.getElementById('settings-window');
     const upgradesContainer = document.getElementById('upgrades-window');
     const travelContainer = document.getElementById('travel-window');
+    const rebirthContainer = document.getElementById('rebirth-window');
 
     const closeSettingsBtn = document.getElementById('closeSettingsBtn');
     const closeUpgradesBtn = document.getElementById('closeUpgradesBtn');
     const closeTravelBtn = document.getElementById('closeTravelBtn');
+    const closeRebirthBtn = document.getElementById('closeRebirthBtn');
 
     const settingsBtn = document.getElementById('settingsBtn');
     const upgradesBtn = document.getElementById('upgradesBtn');
     const travelBtn = document.getElementById('travelBtn');
+    const rebirthBtn = document.getElementById('rebirthBtn');
+
+    const giveBtn = document.getElementById('giveBtn');
 
     // Initially hide other windows
     settingsContainer.style.display = 'none';
     upgradesContainer.style.display = 'none';
     travelContainer.style.display = 'none';
+    rebirthContainer.style.display = 'none';
 
     // Show the main window
     mainContainer.style.display = 'flex';
 
     settingsBtn.addEventListener('click', () => {
+        updateWindowValues(settingsContainer);
         showWindow(settingsContainer);
     });
 
     upgradesBtn.addEventListener('click', () => {
+        updateWindowValues(upgradesContainer);
         showWindow(upgradesContainer);
     });
 
     travelBtn.addEventListener('click', () => {
+        updateWindowValues(travelContainer);
         showWindow(travelContainer);
+    });
+
+    rebirthBtn.addEventListener('click', () => {
+        updateWindowValues(rebirthContainer);
+        showWindow(rebirthContainer);
     });
 
     closeSettingsBtn.addEventListener('click', () => {
@@ -47,12 +61,17 @@ document.addEventListener('DOMContentLoaded', function () {
         hideWindow(travelContainer);
     });
 
+    closeRebirthBtn.addEventListener('click', () => {
+        hideWindow(rebirthContainer);
+    });
+
     function showWindow(container) {
         // Hide all other windows
         mainContainer.style.display = 'none';
         settingsContainer.style.display = 'none';
         upgradesContainer.style.display = 'none';
         travelContainer.style.display = 'none';
+        rebirthContainer.style.display = 'none';
 
         // Show the selected window
         container.style.display = 'flex';
@@ -66,18 +85,40 @@ document.addEventListener('DOMContentLoaded', function () {
         mainContainer.style.display = 'flex';
     }
 
-    // Your existing game logic goes here...
-    let autoClickInterval = 1000;
+    function updateWindowValues(container) {
+        // Update values in the specified container
+        const currencyElement = container.querySelector('#currency');
+        const fuelElement = container.querySelector('#fuel');
+        const energyElement = container.querySelector('#energy');
+
+        currencyElement.textContent = `Currency: ${currency.toFixed(1)}`;
+        fuelElement.textContent = `Fuel: ${fuel.toFixed(1)}`;
+        energyElement.textContent = `Energy: ${energy.toFixed(1)}`;
+    }
 
     let fuel = 0;
     let energy = 0;
     let currency = 0;
+
     let fuelClickVal = 5;
     let enerClickVal = 2;
     let currClickVal = 1;
+
     let fuelMulti = 1;
     let enerMulti = 1;
     let currMulti = 1;
+
+    //upgrade costs
+    let currMultiUpgrade = 100;
+    let autoClickUpgrade = 1000;
+    let autoClick = false;
+    let autoClickRateUpgrade = 500;
+
+    let fuelEffUpgrade = 50;
+    let fuelCapUpgrade = 100;
+
+    let energyEffUpgrade = 50;
+    let energyCapUpgrade = 100;
 
     const currencyElement = document.getElementById('currency');
     const fuelElement = document.getElementById('fuel');
@@ -86,6 +127,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const currMultiBtn = document.getElementById('currMultiBtn');
     const autoClickBtn = document.getElementById('autoClickBtn');
 
+    giveBtn.addEventListener('click', () => {
+        giveResources();
+    });
+
     clickBtn.addEventListener('click', () => {
         fuel += fuelClickVal * fuelMulti;
         energy += enerClickVal * enerMulti;
@@ -93,26 +138,41 @@ document.addEventListener('DOMContentLoaded', function () {
         updateResources();
     });
 
+
+    //----------------------------------------------------------------
     currMultiBtn.addEventListener('click', () => {
-        if (currency >= 100) {
-            currency -= 100;
+        if (currency >= currMultiUpgrade) {
+            currency -= currMultiUpgrade;
+            currMultiUpgrade *= 1.2;
             currMulti += .5;
+            updateWindowValues(upgradesContainer);
             updateResources();
+
         }
     });
 
     autoClickBtn.addEventListener('click', () => {
         if (currency >= 1000) {
             currency -= 1000;
+            autoClick = true;
             autoClick();
             updateResources();
         }
     });
+    //----------------------------------------------------------------
+
 
     function autoClick() {
         autoClickInterval = setInterval(() => {
             clickBtn.click();
         }, 1000);
+    }
+
+    function giveResources() {
+        fuel += 1000000;
+        energy += 1000000;
+        currency += 1000000;
+        updateResources();
     }
 
     function updateResources() {
